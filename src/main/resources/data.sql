0f5e827deac13de43d715372529b55a73aac4159
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-08-26 21:46:04 -0400

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO ROOM (NAME, BED_INFO, NIGHTS, COST) values ('Baymont Mackinaw City', '2Q', 2, 159.00)
---
+++ src/test/java/com/jsaddlercs/vactrac/VacTracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsaddlercs.vactrac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacTracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
