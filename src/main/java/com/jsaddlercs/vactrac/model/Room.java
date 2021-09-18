8135760ee79017669e0fe7cbc1165555f69a6f65
Jonathan A. Saddler <1129384+jazad136@users.noreply.github.com>
2021-09-17 21:54:16 -0400

    Add Hyperlink Column to Room. Update Index

--- src/main/java/com/jsaddlercs/vactrac/model/Room.java
+++ src/main/java/com/jsaddlercs/vactrac/model/Room.java
@@ -24,21 +24,25 @@ public class Room {
 	@Column(name="NIGHTS")
 	private int nights;
 	
-	@Column(name="COST")
+	@Column(name="COST", precision=10, scale=2)
 	private double cost;
 	
 	@Column(name="DATE_OF_STAY")
 	private String dateOfStay;
 	
+	@Column(name="HYPERLINK", length=400)
+	private String hyperlink;
+
 	public Room() {
 	}
 	
-	public Room(String name, String bedInfo, int nights, double cost, String dateOfStay) {
+	public Room(String name, String bedInfo, int nights, double cost, String dateOfStay, String hyperlink) {
 		this.name = name;
 		this.bedInfo = bedInfo;
 		this.nights = nights;
 		this.cost = cost;
 		this.dateOfStay = dateOfStay;
+		this.hyperlink = hyperlink;
 	}
 
 	public long getId() { return id; }
@@ -58,5 +62,12 @@ public class Room {
 	
 	public String getDateOfStay() { return dateOfStay; }
 	public void setDateOfStay(String dateOfStay) { this.dateOfStay = dateOfStay; }
-	
+
+	public String getHyperlink() { return hyperlink; }
+	public void setHyperlink(String hyperlink) { this.hyperlink = hyperlink; }
+
+	public String toString() {
+        return "Room{" + "roomId=" + id + ", " + name +  ", bedInfo=" + bedInfo +
+                ", nights=" + nights + "}\n";
+    }
 }
