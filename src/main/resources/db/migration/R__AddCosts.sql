d23134d5cd53899338e6a98f056f928068a1370f
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-03-26 20:00:34 -0400

    Update Rooms Tables

--- src/main/resources/db/migration/R__AddCosts.sql
+++ src/main/resources/db/migration/R__AddCosts.sql
@@ -1,7 +1,25 @@
-INSERT INTO cost (name, bed_info, price, retrieved_date) values ('La Quinta Ronks', '2Q'              ,119.00, '2022-03-19 00:00:00');
-INSERT INTO cost (name, bed_info, price, retrieved_date) values ('Wyndham Lancaster',  '2Q'           ,139.00, '2022-03-19 00:00:00');
-INSERT INTO cost (name, bed_info, price, retrieved_date) values ('Clarion Inn Strasburg', '2Q'        ,78.00 , '2022-03-19 00:00:00');
-INSERT INTO cost (name, bed_info, price, retrieved_date) values ('Holiday Inn Express Lilitz', '2Q'   ,120.65, '2022-03-23 00:00:00');
-INSERT INTO cost (name, bed_info, price, retrieved_date) values ('Holiday Inn Express Strasburg','2Q' ,118.85, '2022-03-23 00:00:00');
-INSERT INTO cost (name, bed_info, price, retrieved_date) values ('Fairfield Inn Branson', '2Q'        ,131.00, '2022-03-25 00:00:00');
-
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('La Quinta Ronks', '2Q'              ,'0328',119.00, '2022-03-19 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('La Quinta Ronks', '1K'              ,'0328',134.00, '2022-03-19 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('La Quinta Ronks', '2Q'              ,'0328',119.00, '2022-03-21 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('La Quinta Ronks', '1K'              ,'0328',134.00, '2022-03-21 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('La Quinta Ronks', '2Q'              ,'0404',149.00, '2022-03-21 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('La Quinta Ronks', '1K'              ,'0404',164.00, '2022-03-21 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('La Quinta Ronks', '2Q'              ,'0411',134.00, '2022-03-21 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('La Quinta Ronks', '1K'              ,'0411',184.00, '2022-03-21 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('Wyndham Lancaster',  '2Q'           ,'0328',139.00, '2022-03-19 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('Wyndham Lancaster',  '2Q'           ,'0328',139.00, '2022-03-21 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('Wyndham Lancaster',  '2Q'           ,'0411',159.00, '2022-03-21 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('Wyndham Lancaster',  '1K'           ,'0411',149.00, '2022-03-21 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('Clarion Inn Strasburg', '2Q'        ,'0404',78.00 , '2022-03-19 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('Clarion Inn Strasburg', '2Q'        ,'0328',78.00 , '2022-03-21 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('Clarion Inn Strasburg', '1KS'       ,'0328',87.00 , '2022-03-21 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('Clarion Inn Strasburg', '2Q'        ,'0411',80.00 , '2022-03-21 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('Clarion Inn Strasburg', '1KS'       ,'0411',93.00 , '2022-03-21 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('Clarion Inn Strasburg', '2Q'        ,'0418',84.00 , '2022-03-21 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('Clarion Inn Strasburg', '2QS'       ,'0418',103.00, '2022-03-21 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('Clarion Inn Strasburg', '1KS'       ,'0418',98.00 , '2022-03-21 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('Clarion Inn Strasburg', '2Q'        ,'0404',78.00 , '2022-03-23 00:00:00');
+-- end of 03-21 updates
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('Holiday Inn Express Lilitz', '2Q'   ,'0238',120.65, '2022-03-23 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('Holiday Inn Express Strasburg','2Q' ,'0404',118.85, '2022-03-23 00:00:00');
+INSERT INTO cost (name, bed_info, stay_start, price, retrieved_date) values ('Fairfield Inn Branson', '2Q'        ,'0404',131.00, '2022-03-25 00:00:00');
--- src/main/resources/db/migration/V2.1__CreateRoomCostSchema.sql
+++ src/main/resources/db/migration/V2.1__CreateRoomCostSchema.sql
@@ -1,14 +1,15 @@
-CREATE TABLE ROOM(
+CREATE TABLE room(
   room_id BIGSERIAL PRIMARY KEY,
   name varchar(40),
-  bed_info varchar(2),
+  bed_info varchar(3),
   nights int,
   stay_start varchar(4)
 );
 
-CREATE TABLE COST(
+CREATE TABLE cost(
   name varchar(40),
   bed_info varchar(3),
+  stay_start varchar(4),
   price decimal(5, 2),
-  retrieved_date timestamp
-)
+  retrieved_date timestamp 
+);
