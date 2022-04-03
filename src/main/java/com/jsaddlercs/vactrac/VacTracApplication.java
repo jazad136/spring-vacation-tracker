d394443ee0a2753c06314f3d81fca126cf4f8f1e
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-04-02 21:36:47 -0400

    Print all rooms to console as the app starts

--- src/main/java/com/jsaddlercs/vactrac/VacTracApplication.java
+++ src/main/java/com/jsaddlercs/vactrac/VacTracApplication.java
@@ -1,5 +1,7 @@
 package com.jsaddlercs.vactrac;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,20 +11,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 
+import com.jsaddlercs.vactrac.dao.RoomsOnlyDAO;
+import com.jsaddlercs.vactrac.model.Room;
+
 
 @SpringBootApplication
 public class VacTracApplication implements CommandLineRunner {
 
 	@Autowired JdbcTemplate jdbcTemplate;
+	@Autowired RoomsOnlyDAO roomDAO;
 	public static void main(String[] args) {
 		SpringApplication.run(VacTracApplication.class, args);
 	}
 
 	private static final Logger LOG = LoggerFactory.getLogger(VacTracApplication.class);
+	
 	public void run(String... args) throws Exception {
-		String sql = "SELECT 1;";         
-        SqlRowSet srs = jdbcTemplate.queryForRowSet(sql);
-        srs.next();
-        LOG.info("Queried and got " + srs.getInt(1));
+		
+		List<Room> roomResult = roomDAO.getAllRooms();
+		roomResult.forEach(System.out::println);
+//		String sql = "SELECT 1;";         
+//        SqlRowSet srs = jdbcTemplate.queryForRowSet(sql);
+//        srs.next();
+//        LOG.info("Queried and got " + srs.getInt(1));
+		
     }
 }
