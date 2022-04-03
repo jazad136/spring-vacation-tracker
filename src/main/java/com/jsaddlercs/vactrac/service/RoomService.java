44a99d2f0360539f93877de0816a8b78c2cf7b7b
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-04-03 14:34:16 -0400

    Add getRoomsWCosts to Controller for getting rooms with costs

--- src/main/java/com/jsaddlercs/vactrac/service/RoomService.java
+++ src/main/java/com/jsaddlercs/vactrac/service/RoomService.java
@@ -1,43 +1,31 @@
 package com.jsaddlercs.vactrac.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.sql.DataSource;
-
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import com.jsaddlercs.vactrac.dao.CostDAO;
+import com.jsaddlercs.vactrac.dao.RoomsOnlyDAO;
 import com.jsaddlercs.vactrac.model.Room;
+import com.jsaddlercs.vactrac.model.RoomCost;
 
 @Service
 public class RoomService {
-
-//	private static final List<Room> rooms = new ArrayList<>();
-//    static{
-//        for(long i = 0; i < 3; i++){
-//            rooms.add(new Room(i, "Baymont Mackinaw City","2Q", 2, 159.00, "0831"));
-//        }
-//    	int i = 1;
-//    	rooms.add(new Room(i, "Baymont Mackinaw City","2Q", 2, 159.00));
-//    	i++;
-//    	rooms.add(new Room(i, "Baymont Mackinaw City","2Q", 2, 159.00));
-//    	i++;
-//    	rooms.add(new Room(i, "Baymont Mackinaw City","2Q", 2, 159.00));
-    	
-//    }
-
-//    public List<Room> getAllRooms(){
-//        return rooms;
-//    }
-	private final JdbcTemplate jdbcTemplate;
-
-	public RoomService(DataSource dataSource) {
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	
+	private final RoomsOnlyDAO roomsDAO;
+	private final CostDAO costDAO;
+	
+	public RoomService(RoomsOnlyDAO roomsDAO, CostDAO costDAO) {
+		this.roomsDAO = roomsDAO;
+		this.costDAO = costDAO;
 	}
 
 	public List<Room> getAllRooms() { 
-		return new ArrayList<>();
+		return roomsDAO.getAllRooms();
+	}
+	
+	public List<RoomCost> getAllRoomsWCosts() { 
+		return costDAO.getAllRoomsWCosts();
 	}
 	
 }
